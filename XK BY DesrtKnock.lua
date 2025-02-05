0b731b3e9263b4c8743c43eb98818fb4e2c75db6
DesrtKnock <2046738141@qq.com>
2025-02-05 19:35:48 +0800

    Add files via upload

--- XK BY DesrtKnock.lua
+++ XK BY DesrtKnock.lua
@@ -1,9 +1,9 @@
 local AA = 35588119
 local BB = game.Players.LocalPlayer 
 if BB:IsInGroup(AA) then 
-    SansHubESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/DesrtKnock/DesrtKnock-X/refs/heads/main/XK.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DesrtKnock/DesrtKnock-X/refs/heads/main/XK.lua"))()
     else 
-  BB:Kick(请加入群组，已自动为您复制群组链接)
+  game.Players.LocalPlayer:Kick('请加入群组，已自动为您复制群组链接')
   setclipboard("https://www.roblox.com/communities/35588119/XK-AND-XRX#!/about")
   setclipboard("群组名字XK AND XRX工作室")
 end
